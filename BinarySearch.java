288c7afd578ef5226810f5901d68956fd9a7cc03
mohd faizan <faizanali331@gmail.com>
2024-08-21 00:28:23 +0530

    binaray search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class BinarySearch {
+class BinarySearch{
+    public int search(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) j=mid-1;
+            else i=mid+1;
+        }
+        return -1;
+    }
 }
