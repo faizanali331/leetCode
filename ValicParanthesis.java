74502a4508938b9230f760e14906768a6336eabf
mohd faizan <faizanali331@gmail.com>
2024-08-21 11:17:53 +0530

    valid paranthesis or balance brackets

--- ValicParanthesis.java
+++ ValicParanthesis.java
@@ -1,2 +1,31 @@
-public class ValicParanthesis {
+import java.util.Stack;
+
+class ValicParanthesis {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        char[] ca = s.toCharArray();
+        for(char c: ca){
+
+            if(c=='('||c=='{'||c=='[')
+                stk.push(c);
+            else if(stk.isEmpty()) return false;
+            else{
+                if(c==')'){
+                    if(stk.peek()=='(')
+                        stk.pop();
+                    else return false;
+                }else if(c=='}'){
+                    if(stk.peek()=='{')
+                        stk.pop();
+                    else return false;
+                }else{
+                    if(stk.peek()=='[')
+                        stk.pop();
+                    else return false;
+                }
+            }
+        }
+        if(!stk.isEmpty()) return false;
+        return true;
+    }
 }
