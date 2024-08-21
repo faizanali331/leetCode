3008df0e3d6f52a16709780bd99cb106fe6f3ae0
mohd faizan <faizanali331@gmail.com>
2024-08-21 11:08:38 +0530

    sum of two integers to get target return index

--- SumOfTwoIntegers.java
+++ SumOfTwoIntegers.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class SumOfTwoIntegers {
+class SumOfTwoIntegers{
+    public int[] twoSum(int[] nums, int target) {
+        int []b = {0, 0};
+        for(int i=0; i<nums.length-1; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j]==target){
+                    int []a = {i, j};
+                    return a;
+                }
+
+            }
+        }
+
+        return b;
+    }
 }
